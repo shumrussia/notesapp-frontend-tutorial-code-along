951148652980b03d5f77af96d5e35ca7d28a6c5e
shumrussia <azat.nuriakhmetov@gmail.com>
2021-12-28 13:10:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import NotesList from './components/NotesList';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/" component={NotesList} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
